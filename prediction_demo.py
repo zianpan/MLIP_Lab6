6d75f2340e39ced18dea82edaefd63e8725b273c
Zekai Li <zekail@andrew.cmu.edu>
2024-02-15 03:39:29 -0500

    Update prediction demo

---
+++ prediction_demo.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+def data_preparation(df: pd.DataFrame):
+    # select target column and feature columns
+    target = "price"
+    features = ["furnishingstatus"]
+
+    feature_df = pd.pandas. \
+        get_dummies(df,columns=["furnishingstatus"]). \
+        loc[:,features]
+
+    target_series = df.loc[:,target]
+
+    return feature_df, target_series
+
+def data_split(features: pd.DataFrame, target:pd.Series):
+    X_train, X_test, y_train, y_test = \
+        train_test_split(features, 
+                         target, 
+                         test_size=0.33, 
+                         random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def train_model(X_train: np.ndarray, 
+                y_train: np.ndarray):
+    reg = LinearRegression().fit(X_train, y_train)
+    return reg
+
+def eval_model(X_test: np.ndarray,
+               y_test: np.ndarray,
+               model: LinearRegression):
+    return model.score(X_test,y_test)
+
+if __name__ == "__main__":
+    df_raw = pd.read_csv('Housing.csv')
+    feature_df, target_series = data_preparation(df_raw)
+    X_train, X_test, y_train, y_test = data_split(feature_df, target_series)
+    reg=train_model(X_train,y_train)
+    eval_score=eval_model()
+    print(f"Trained model score is: {eval_score}")
+
--- test.py
+++ test.py
@@ -1,8 +1,41 @@
 import pytest
 import pandas as pd
+import numpy as np
+from prediction_demo import data_preparation,data_split,train_model,eval_model
 
 @pytest.fixture
 def housing_data_stub():
     return pd.Dataframe(
-      {price	area	bedrooms	bathrooms	stories	mainroad	guestroom	basement	hotwaterheating	airconditioning	parking	prefarea	furnishingstatus}
+      data ={
+      'price':[13300000,12250000],
+      'area':[7420,8960],
+    	'bedrooms':[4,4],	
+      'bathrooms':[2,4],	
+      'stories':[3,4],	
+      'mainroad':["yes","yes"],	
+      'guestroom':["no","no"],	
+      'basement':["no","no"],	
+      'hotwaterheating':["no","no"],	
+      'airconditioning':["yes","yes"],	
+      'parking':[2,3],
+      'prefarea':["yes","no"],	
+      'furnishingstatus':["furnished","unfurnished"]}
     )
+
+def test_data_preparation(housing_data_stub):
+    feature_df, target_series = data_preparation(housing_data_stub)
+    # Target and datapoints has same length
+    assert feature_df.shape[0]==len(target_series)
+
+    #Feature only has numerical values
+    assert feature_df.shape[1] == feature_df.select_dtypes(include=np.number).shape[1]
+
+@pytest.fixture
+def feature_target_stub(housing_data_stub):
+    feature_df, target_series = data_preparation(housing_data_stub)
+    return (feature_df, target_series)
+
+def test_data_split(feature_target_stub):
+    return_tuple = data_split(*feature_target_stub)
+    # TODO test if the length of return_tuple is 4
+    raise NotImplemented
